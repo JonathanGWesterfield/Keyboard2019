07bd48c85d87daf338b1fcea0b4ae47b164ad8da
JonathanGWesterfield <jgwesterfield@gmail.com>
2019-12-27 23:30:31 -0600

    Added the microcontroller files for my latest layout

---
+++ Micro controller Files/keyboard/gh60/cmake-build-debug/CMakeFiles/gh60.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gh60.dir/keymap_poker.c.o"
+  "CMakeFiles/gh60.dir/matrix.c.o"
+  "gh60.pdb"
+  "gh60"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gh60.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
